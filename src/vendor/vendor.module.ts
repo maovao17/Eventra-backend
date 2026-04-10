45cc091c86e0fca06be62305e6f848cfbf667437
Maithalee Vishe <maithaleevishe@Maithalees-MacBook-Air.local>
2026-04-10 09:07:19 +0530

    fix

--- src/vendor/vendor.module.ts
+++ src/vendor/vendor.module.ts
@@ -3,9 +3,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { VendorController } from './vendor.controller';
 import { VendorService } from './vendor.service';
 import { Vendor, VendorSchema } from './schemas/vendor.schema';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Vendor.name, schema: VendorSchema }])],
+  imports: [MongooseModule.forFeature([{ name: Vendor.name, schema: VendorSchema }]), UserModule],
   controllers: [VendorController],
   providers: [VendorService],
   exports: [VendorService],
--- src/vendor/vendor.service.ts
+++ src/vendor/vendor.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { UpdateVendorDto } from './dto/update-vendor.dto';
 import { Vendor, VendorDocument } from './schemas/vendor.schema';
+import { UserModule } from '../user/user.module';
 
 @Injectable()
 export class VendorService {
